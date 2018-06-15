20a3f284c2583f0f3ff032a929b23195013638bf
nanzheng <nanzheng@freedotech.com>
2018-06-15 10:31:25 +0800

    IU-2018.1.5 <南征@DESKTOP-BBU00EN Overwrite remote http://bitbucket.gbim.cc/scm/buildingguard/cloudlink-building-guard.git to local

--- src/main/java/com/freedoonline/domain/DomainDao.java
+++
@@ -1,9 +0,0 @@
-package com.freedoonline.domain;
-
-import java.util.Map;
-
-public interface DomainDao {
-	
-	//获取阈值列表
-	Object queryThreshold(Map<String, Object> paramMap);
-}
--- src/main/java/com/freedoonline/domain/EquSystemDao.java
+++
@@ -1,9 +0,0 @@
-package com.freedoonline.domain;
-
-import com.freedoonline.domain.entity.EquSystem;
-
-public interface EquSystemDao {
-	
-	// 获取系统列表
-	public Object querySystemList(EquSystem system);
-}
